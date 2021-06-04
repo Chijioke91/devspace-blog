002f241c4f1a8597aebe8c29a20f101e38713bbb
cjaytechguy <cjaytechguy@gmail.com>
2021-06-04 18:14:47 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
 import Layout from '../components/Layout';
+import Post from '../components/Post';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Layout title="Home">
-      <h1>Home Page</h1>
+      <h1 className="text-4xl border-b-4 p-5 font-bold">Latest Posts</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, idx) => {
+          return <Post key={idx} {...post} />;
+        })}
+      </div>
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">All Posts</a>
+      </Link>
     </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync(path.join(process.cwd(), 'posts'));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+
+    const metaMark = fs.readFileSync(path.join(process.cwd(), 'posts', filename), 'utf8');
+
+    const { data: frontMatter } = matter(metaMark);
+
+    return { slug, frontMatter };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
